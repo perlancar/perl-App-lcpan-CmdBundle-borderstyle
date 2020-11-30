ef8ca43eef9be94280f78d1582f4bbddfa5d748a
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-11-30 14:26:24 +0700

    Initial commit

---
+++ lib/App/lcpan/CmdBundle/borderstyle.pm
@@ -0,0 +1,31 @@
+package App::lcpan::CmdBundle::borderstyle;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: lcpan subcommands related to BorderStyle
+
+=head1 SYNOPSIS
+
+Install this distribution, then the lcpan subcommands below will be available:
+
+ # List BorderStyle modules available on CPAN
+ % lcpan borderstyle-mods
+
+
+=head1 DESCRIPTION
+
+This distribution packages several lcpan subcommands related to
+L<BorderStyle>. More subcommands will be added in future releases.
+
+Some ideas:
+
+
+=head1 SEE ALSO
+
+L<lcpan>
+
+L<BorderStyle>
